cf84f575e7d838044f8d44c136d9d5c27a9a28b6
166660614 <166660614@qq.com>
2019-03-15 11:03:21 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+$router->post('/user/login','Users\UserController@login');//登录接口
+$router->post('/user/center','Users\UserController@center');//用户中心接口
+$router->post('/user/order','Users\UserController@order');//防刷
